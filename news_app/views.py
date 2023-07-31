cc7cfa7691138262e97f6a213b4eda2e4c90264d
Abhishek Dangi <dangiabhi332@gmail.com>
2023-07-31 16:47:28 +0545

    news

--- news_app/views.py
+++ news_app/views.py
@@ -24,24 +24,14 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["featured_post"] = (
-            Post.objects.filter(published_at__isnull=False, status="active")
-            .order_by("-published_at", "views_count")
-            .first()
-        )
-        context["featured_posts"] = Post.objects.filter(
-            published_at__isnull=False, status="active"
-        ).order_by("-published_at", "views_count")[1:4]
+        context["featured_post"] = Post.objects.filter(published_at__isnull=False, status="active").order_by("-published_at", "views_count").first()
+        context["featured_posts"] = Post.objects.filter(published_at__isnull=False, status="active").order_by("-published_at", "views_count")[1:4]
         one_week_ago = timezone.now() - timedelta(days=7)
-        context["weekly_top_posts"] = Post.objects.filter(
-            published_at__isnull=False, status="active", published_at__gte=one_week_ago
-        ).order_by("-published_at", "-views_count")[:7]
-        context["recent_posts"] = Post.objects.filter(
-            published_at__isnull=False, status="active"
-        ).order_by("-published_at")[:7]
+        context["weekly_top_posts"] = Post.objects.filter(published_at__isnull=False, status="active", published_at__gte=one_week_ago).order_by("-published_at", "-views_count")[:7]
+        context["recent_posts"] = Post.objects.filter(published_at__isnull=False, status="active").order_by("-published_at")[:7]
         return context
-
-
+    
+      
 class AboutView(TemplateView):
     template_name = "aznews/about.html"
 
