c073969f69fce9db76a1f7785c826fa10f98d3a2
Abhishek Dangi <dangiabhi332@gmail.com>
2023-08-12 23:02:01 +0545

    feat:newspaper api

--- Newspaper/urls.py
+++ Newspaper/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('news_admin/', include("blog_app.urls")),
+    path("api/v1/",include("API.urls")),
     path("", include("news_app.urls")),
     path("account/login/", views.LoginView.as_view(), name="login"),
     path("account/logout/", views.LogoutView.as_view(), name="logout")
